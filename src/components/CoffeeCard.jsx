616e642d34bbf45cbf6f77dbadeda4ca428d854b
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-04-23 16:45:37 +0600

    delete client side data

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,39 @@
 import PropTypes from 'prop-types';
+import Swal from 'sweetalert2';
 
-const CoffeeCard = ({card}) => {
+const CoffeeCard = ({ card }) => {
     // console.log(card);
+    const DeleteHandle = id => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffee/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+
+                    })
+
+            }
+        });
+    }
     return (
         <div className="card card-side bg-base-100 p-2 shadow-xl">
             <figure className='w-1/3'><img className='w-full rounded-lg' src={card.photo} alt="Movie" /></figure>
@@ -11,7 +43,7 @@ const CoffeeCard = ({card}) => {
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">edit</button>
                     <button className="btn btn-primary">see</button>
-                    <button className="btn btn-primary">delete</button>
+                    <button onClick={() => DeleteHandle(card._id)} className="btn btn-primary">delete</button>
                 </div>
             </div>
         </div>
@@ -21,5 +53,5 @@ const CoffeeCard = ({card}) => {
 export default CoffeeCard;
 
 CoffeeCard.propTypes = {
-    card: PropTypes.array.isRequired,
+    card: PropTypes.object.isRequired,
 }
